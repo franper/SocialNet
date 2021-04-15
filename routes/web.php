f7e8c603ef27113f46b8b143a281b5341061c2ec
= <=>
2021-04-15 15:49:44 +0200

    se ha implementado la funcionalidad de autenticacion

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//use App\Image;
 
 Route::get('/', function () {
+    /*
+    $images = Image::all();
+
+    foreach($images as $image){
+        echo $image->image_path.'<br>';
+        echo $image->description.'<br>'; 
+        echo $image->user->name.' '.$image->user->surname.'<br>'; 
+        echo '<strong>Likes<strong> '.count($image->likes);
+        if(count($image->comments) >= 1){
+            echo '<h4>Comentarios: </h4>';
+            foreach($image->comments as $comment){
+                echo '<strong>'.$comment->user->name.' '.$comment->user->surname.'</strong> '; 
+                echo $comment->content.'<br>';
+            }
+        }
+        
+        
+        echo '<hr>';
+    
+    }
+
+    die();*/
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
